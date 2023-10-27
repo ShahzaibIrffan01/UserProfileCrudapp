631b888cbd5310e9d0fc02810e9fcd8d83aa4ae2
tanveerAnjum <tanveerdar933@gmail.com>
2023-10-27 17:39:49 +0500

    repo cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import './App.css';
 import React from 'react';
-import UserProfileCard from './UserProfileCard';
+import UserProfileCard from './Components/UserProfileCard';
 
 
 function App() {
