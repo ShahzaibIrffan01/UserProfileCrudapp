e0724fa1eb4abc55f2096a63f913a9b4556e003c
tanveer anjum <tanveerdar933@gmail.com>
2023-10-27 19:32:47 +0500

    working on integrating post and get api's

--- src/Components/UserProfileCard.js
+++ src/Components/UserProfileCard.js
@@ -1,100 +1,132 @@
-import React ,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import './UserProfileCard.css'
 // import Data from "./CardData";
 
 function UserProfileCard() {
-  const [userData, setUserData] = useState([]);
-     const [name, setName] = useState('');
-     const [age, setAge] = useState('');
-     const [email, setEmail] = useState('');
-     const [profilePhoto, setProfilePhoto] = useState('');
- 
-     useEffect(() => {
-     
-       console.log('User data updated:', userData);
-         }, [userData]);
- 
-     const addUser = () => {
-       if (name && age && email && profilePhoto) {
-         const newUser = {
-           name,
-           age,
-           email,
-           profilePhoto,
-         };
- 
-         setUserData([...userData, newUser]);
-   
-         setName('');
-         setAge('');
-         setEmail('');
-         setProfilePhoto('');
-       } else {
-         alert('Please fill in all fields.');
-       }
-     };
- 
-     const removeUser = (index) => {
-       const updatedData = [...userData];
-       updatedData.splice(index, 1);
-       setUserData(updatedData);
-     };
-  return(
+  const [userObj, setUserObj] = useState({});
+  const [usersList, setUsersList] = useState([]);
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [email, setEmail] = useState('');
+  const [profilePhoto, setProfilePhoto] = useState('');
+
+
+
+  useEffect(() => {
+    postUser("http://localhost:8000/user/add", userObj)
+      .then(res => alert(res))
+      .catch(err => console.log(err))
+  }, [userObj]);
+
+  const getUsers = async (url) => {
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        mode: "cors"
+      })
+      console.log(response.json());
+      return response.json();
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getUsers("http://localhost:8000/user/get")
+      .then(res => setUsersList(res))
+      .catch(err => console.log(err))
+  }, [])
+
+  const postUser = async (url, data) => {
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json"
+          // 'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: JSON.stringify(data)
+      })
+      return response.json();
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleAddProfile = () => {
+    if (name && age && email && profilePhoto) {
+      setUserObj({ name, age, email, picture: profilePhoto, })
+    } else {
+      alert('Please fill in all fields.');
+    }
+  };
+
+  // const removeUser = (index) => {
+  //   const updatedData = [...userObj];
+  //   updatedData.splice(index, 1);
+  //   setUserObj(updatedData);
+  // };
+  return (
     <div className="cardcontainer">
       <div className="inputs">
         <form className="form">
 
-      <div>
-           <label>Name: </label>
-           <input type="text" className="name" value={name} onChange={(e) => setName(e.target.value)} />
-         </div>
-         <div>
-           <label>Age : </label>
-           <input type="number" value={age} className="age" onChange={(e) => setAge(e.target.value)} />
-         </div>
-         <div>
-           <label>Email: </label>
-           <input type="text" className="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-         </div>
-         <div>
-           <label>Picture: </label>
-           <input type="text" className="image" value={profilePhoto} onChange={(e) => setProfilePhoto(e.target.value)} />
-        </div>
-         <button className='btn1' type="button" onClick={addUser}>Add User</button>
+          <div>
+            <label>Name: </label>
+            <input type="text" className="name" value={name} onChange={(e) => setName(e.target.value)} />
+          </div>
+          <div>
+            <label>Age : </label>
+            <input type="number" value={age} className="age" onChange={(e) => setAge(e.target.value)} />
+          </div>
+          <div>
+            <label>Email: </label>
+            <input type="text" className="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div>
+            <label>Picture: </label>
+            <input type="text" className="image" value={profilePhoto} onChange={(e) => setProfilePhoto(e.target.value)} />
+          </div>
+          <button className='btn1' type="button" onClick={handleAddProfile}>Add User</button>
         </form>
       </div>
-     
-      
-      
+
+
+
       <div className="output">
-        
+
         <div className="card">
 
-      <ul>
-           {userData.map((user, index) => (
-             
-             <li key={index}>
-              <div className="cardbak">
-              <div className="image">
-               <img className="image1" src={user.profilePhoto} alt={`${user.name}'s profile`} />
-              </div>
-              
-               <div className="cardcontent">
-                 <h2>{user.name}</h2>
-                 <p>Age: {user.age}</p>
-                 <p>Email: {user.email}</p>
-               <button className="btn" onClick={() => removeUser(index)}>Remove</button>
-               </div>
-              </div>
-             </li>
-            
-            ))}
-         </ul>
-            </div>
+          <ul>
+            {usersList.length ?
+              usersList.map((user, index) => (
+
+                <li key={index}>
+                  <div className="cardbak">
+                    <div className="image">
+                      <img className="image1" src={user.profilePhoto} alt={`${user.name}'s profile`} />
+                    </div>
+
+                    <div className="cardcontent">
+                      <h2>{user.name}</h2>
+                      <p>Age: {user.age}</p>
+                      <p>Email: {user.email}</p>
+                      {/* <button className="btn" onClick={() => removeUser(index)}>Remove</button> */}
+                    </div>
+                  </div>
+                </li>
+              ))
+              : <p>no data</p>
+            }
+          </ul>
+        </div>
       </div>
-      
-      
-     
+
+
+
     </div> //card container
   )
 }
